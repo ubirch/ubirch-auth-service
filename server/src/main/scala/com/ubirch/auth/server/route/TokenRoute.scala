7598b4ef756e5687fce03d594ac453e76ca8ad6f
cvandrei <crille@indarium.de>
2017-02-01 11:19:36 +0100

    changed error message text

--- server/src/main/scala/com/ubirch/auth/server/route/TokenRoute.scala
+++ server/src/main/scala/com/ubirch/auth/server/route/TokenRoute.scala
@@ -44,7 +44,7 @@ trait TokenRoute extends MyJsonProtocol
           entity(as[AfterLogin]) { afterLogin =>
             onSuccess(tokenActor ? afterLogin) {
               case res: AfterLogin => complete(afterLogin)
-              case _ => complete(serverErrorResponse(errorType = "VerificationError", errorMessage = "failed to verify token"))
+              case _ => complete(serverErrorResponse(errorType = "VerificationError", errorMessage = "failed to verify code"))
             }
           }
         }
