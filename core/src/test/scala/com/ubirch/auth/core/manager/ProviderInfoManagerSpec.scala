1450ed51f5614f0eae7d37afb298705e43d10c7a
cvandrei <crille@indarium.de>
2017-03-14 15:06:06 +0100

    UBD-288: begin with test case description

---
+++ core/src/test/scala/com/ubirch/auth/core/manager/ProviderInfoManagerSpec.scala
@@ -0,0 +1,47 @@
+package com.ubirch.auth.core.manager
+
+import org.scalatest.{BeforeAndAfterEach, FeatureSpec, Matchers}
+
+/**
+  * author: cvandrei
+  * since: 2017-03-14
+  */
+class ProviderInfoManagerSpec extends FeatureSpec
+  with Matchers
+  with BeforeAndAfterEach {
+
+  feature("providerInfoList()") {
+
+    // TODO define test cases
+
+    ignore("context does not exist") {
+      // TODO
+    }
+
+    ignore("context exists and has providers --> return providers") {
+      // TODO
+    }
+
+    ignore("context exists but without providers (none configured) --> return no providers") {
+      // TODO
+    }
+
+    ignore("context exists but without providers (none activated) --> return no providers") {
+      // TODO
+    }
+
+    ignore("context exists and has (inactive) providers --> return no providers") {
+      // TODO
+    }
+
+    ignore("context exists, provider is enabled but provider config is missing --> return no providers") {
+      // TODO
+    }
+
+    ignore("context exists and has providers but context is not enabled --> return no providers") {
+      // TODO
+    }
+
+  }
+
+}
