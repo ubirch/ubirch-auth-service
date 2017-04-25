a4f137f0bfd7edce1ab4e1b2e7c9e5169ac6e40c
cvandrei <crille@indarium.de>
2017-04-25 14:27:24 +0200

    UBD-497: implemented UserInfoManager#getInfo

--- core/src/main/scala/com/ubirch/auth/core/manager/UserInfoManager.scala
+++ core/src/main/scala/com/ubirch/auth/core/manager/UserInfoManager.scala
@@ -1,6 +1,8 @@
 package com.ubirch.auth.core.manager
 
-import com.ubirch.auth.model.{UserInfo, UserUpdate}
+import com.ubirch.auth.model.{UserInfo, UserInfoGroup, UserUpdate}
+import com.ubirch.user.core.manager.{GroupsManager, UserManager}
+import com.ubirch.user.model.db.{Group, User}
 import com.ubirch.util.mongo.connection.MongoUtil
 import com.ubirch.util.oidc.model.UserContext
 
@@ -16,8 +18,36 @@ object UserInfoManager {
   def getInfo(userContext: UserContext)
              (implicit mongo: MongoUtil): Future[Option[UserInfo]] = {
 
-    // TODO implement
-    Future(None)
+    // TODO automated tests
+    UserManager.findByProviderIdAndExternalId(
+      providerId = userContext.providerId,
+      externalUserId = userContext.userId
+    ) flatMap {
+
+      case None => Future(None)
+
+      case Some(user: User) =>
+
+        GroupsManager.findByContextAndUser(
+          contextName = userContext.context,
+          providerId = userContext.providerId,
+          externalUserId = userContext.userId
+        ) map { groups =>
+
+          val myGroups = groups filter(_.ownerId == user.id)
+          val allowedGroups = groups diff myGroups
+
+          val info = UserInfo(
+            displayName = user.displayName,
+            myGroups = toUserInfoGroups(myGroups),
+            allowedGroups = toUserInfoGroups(allowedGroups)
+          )
+          Some(info)
+
+        }
+
+
+    }
 
   }
 
@@ -27,6 +57,12 @@ object UserInfoManager {
     // TODO implement
     Future(None)
 
+  }
+
+  private def toUserInfoGroups(groups: Set[Group]): Set[UserInfoGroup] = {
+    groups map { group =>
+      UserInfoGroup(group.id, group.displayName)
+    }
   }
 
 }
