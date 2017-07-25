6217dfd55b3f95e38469a91f7b64daec015333bf
derMicha <dermicha@indarium.de>
2017-07-25 15:30:26 +0200

    dev/demo fixed for trackle

--- util/src/main/scala/com/ubirch/auth/util/db/config/defaults/OidcContextProvider.scala
+++ util/src/main/scala/com/ubirch/auth/util/db/config/defaults/OidcContextProvider.scala
@@ -49,7 +49,7 @@ object OidcContextProvider {
   )
 
   private lazy val trackleUILocalGoogle: ContextProviderConfig = ContextProviderConfig(
-    context = ContextDefinitions.ctxTrackleAdminUILocal,
+    context = ContextDefinitions.ctxTrackleUILocal,
     provider = OidcProviders.providerIdGoogle,
     clientId = "370115332091-dqhiaemv68bjvtnp84beg26plrpkmc8t.apps.googleusercontent.com",
     clientSecret = "n3fPuja818436VmggJZSht6-",
@@ -69,7 +69,7 @@ object OidcContextProvider {
   )
 
   private lazy val trackleAdminUIDevGoogle: ContextProviderConfig = ContextProviderConfig(
-    context = ContextDefinitions.ctxTrackleAdminUIDev,
+    context = ContextDefinitions.ctxTrackleAdminUIDemo,
     provider = OidcProviders.providerIdGoogle,
     clientId = "370115332091-6iu7qbha7f5oo8d12tsteh5crsj53m9k.apps.googleusercontent.com",
     clientSecret = "", // TODO set client secret
