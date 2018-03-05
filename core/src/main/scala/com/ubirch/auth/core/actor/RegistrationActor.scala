8a6e5ce294fe5c19fc35a6f0f1139d708ea0bed8
derMicha <dermicha@dermicha.de>
2018-03-05 10:55:55 +0100

    fixed logging may be enhanced registrationn error handling

--- core/src/main/scala/com/ubirch/auth/core/actor/RegistrationActor.scala
+++ core/src/main/scala/com/ubirch/auth/core/actor/RegistrationActor.scala
@@ -1,15 +1,15 @@
 package com.ubirch.auth.core.actor
 
+import akka.actor.{Actor, ActorLogging, Props}
+import akka.routing.RoundRobinPool
 import com.ubirch.auth.config.Config
 import com.ubirch.auth.core.manager.RegistrationManager
 import com.ubirch.util.model.JsonErrorResponse
 import com.ubirch.util.mongo.connection.MongoUtil
 import com.ubirch.util.oidc.model.UserContext
 
-import akka.actor.{Actor, ActorLogging, Props}
-import akka.routing.RoundRobinPool
-
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
 /**
   * author: cvandrei
@@ -22,7 +22,20 @@ class RegistrationActor(implicit mongo: MongoUtil) extends Actor
 
     case register: RegisterUser =>
       val sender = context.sender()
-      RegistrationManager.register(register.userContext) map (sender ! _)
+      RegistrationManager.register(register.userContext).onComplete {
+        case Success(ui) if ui.isDefined =>
+          sender ! ui.get
+        case Success(ui) =>
+          sender ! JsonErrorResponse(
+            errorType = "ValidationError",
+            errorMessage = s"Registration failed for ${register.userContext.userId}"
+          )
+        case Failure(t) =>
+          sender ! JsonErrorResponse(
+            errorType = "ValidationError",
+            errorMessage = t.getMessage
+          )
+      }
 
     case _ =>
       log.error("unknown message")
